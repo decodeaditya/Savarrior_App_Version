887a838ba8d6009f195b467bcac3643166b48797
Aditya <adityaj02810@gmail.com>
2023-05-20 15:20:55 +0530

    reset-link-update

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -8,7 +8,7 @@ import Button from '../components/Button'
 import { Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { auth } from '../firebase'
-import { deleteUser, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { deleteUser, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const LoginScreen = () => {
@@ -55,11 +55,35 @@ const LoginScreen = () => {
     }
 
 
+    const handleChangePassword = () => {
+      if(emailRegex.test(email)){
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            Alert.alert('Reset Link Sent',"Password Reset Link Sent to Your Email",[
+                {text:'OK'}
+            ])
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            Alert.alert(errorCode.split("/")[1],null,[
+                {text:'OK'}
+            ])
+        });
+      }
+      else{
+        Alert.alert('Invalid Email','Enter A Valid Email',[
+            {text:'OK'}
+        ])
+      }
+    }
+
+
     return (
         <>
             <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
                 <View style={{ backgroundColor: colors.white, paddingHorizontal: 20, justifyContent: 'center', height: "100%" }}>
-                    <View style={{ padding: 5, paddingHorizontal: 30, backgroundColor: colors.white,alignItems:"center" }}>
+                    <View style={{ padding: 5, paddingHorizontal: 30, backgroundColor: colors.white, alignItems: "center" }}>
                         <Image source={require("../assets/logo-circle.png")} style={{ width: screenWidth * 0.24, height: screenWidth * 0.24 }} />
                     </View>
                     <View>
@@ -82,15 +106,18 @@ const LoginScreen = () => {
                             <TextInput style={{ fontSize: 18, fontFamily: FontVariants.weight500, width: "90%", color: colors.fontGray }} placeholder='Enter Password' secureTextEntry={true} value={code} onChangeText={setCode} />
                         </View>
                     </View>
-                    <View style={{ marginTop: 40, marginHorizontal: 10 }}>
+                    <Pressable style={{ padding: 10, marginTop: 1 }} onPress={handleChangePassword}>
+                        <Text style={{ textAlign: "right", fontFamily: FontVariants.weight500, fontSize: 16, textDecorationLine: "underline", color: colors.fontGray }}>Forgot Password?</Text>
+                    </Pressable>
+                    <View style={{ marginTop: 20, marginHorizontal: 10, marginBottom: 10 }}>
                         <Button title='Login' style={{ borderRadius: 10, paddingVertical: 15 }} onPress={handleSubmit} textStyle={{ color: colors.white }} />
                     </View>
-                    {/* <View style={{ padding: 15, flexDirection: 'row', alignItems: "center", justifyContent: "center", marginVertical: 10 }}>
-                      <View style={{ borderTopWidth: 1, borderTopColor: '#d3d3d3', width: screenWidth/4 }} />
-                      <Text style={{ fontFamily: FontVariants.weight600, fontSize: 18, marginHorizontal: 7, color:colors.fontGray, textAlign: "center" }}>OR</Text>
-                      <View style={{ borderTopWidth: 1, borderTopColor: '#d3d3d3', width: screenWidth/4 }} />
-                  </View>
-
+                    {/* <View style={{flexDirection: 'row', alignItems: "center", justifyContent: "center", marginVertical: 15 }}>
+                        <View style={{ borderTopWidth: 1, borderTopColor: '#d3d3d3', width: screenWidth / 4 }} />
+                        <Text style={{ fontFamily: FontVariants.weight600, fontSize: 18, marginHorizontal: 7, color: colors.fontGray, textAlign: "center" }}>OR</Text>
+                        <View style={{ borderTopWidth: 1, borderTopColor: '#d3d3d3', width: screenWidth / 4 }} />
+                    </View> */}
+                    {/*
                   <Pressable>
                   <View
                           style={{
@@ -101,8 +128,8 @@ const LoginScreen = () => {
                           <Text style={{fontFamily:FontVariants.weight600,fontSize:18,marginLeft:10}}>Countinue with Google</Text>
                       </View>
                   </Pressable> */}
-                    <Pressable style={{ padding: 10, marginTop: 10 }} onPress={() => navigation.navigate("Register")}>
-                        <Text style={{ textAlign: "center", fontFamily: FontVariants.weight600, fontSize: 16 }}>Don't Have A Account? <Text style={{ color: colors.fontGray, textDecorationColor: colors.black, textDecorationLine: "underline" }}>Sign Up</Text></Text>
+                    <Pressable style={{ padding: 2 }} onPress={() => navigation.navigate("Register")}>
+                        <Text style={{ textAlign: "center", fontFamily: FontVariants.weight600, fontSize: 16 }}>Don't have an account? <Text style={{ color: colors.fontGray, textDecorationColor: colors.black, textDecorationLine: "underline" }}>Sign Up</Text></Text>
                     </Pressable>
                 </View>
             </SafeAreaView>
